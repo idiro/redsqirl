053ec043c0554e02e4b3e94a08ace16cc1d0066b
marcos <marcos@marcos-pc>
2013-10-22 11:26:14 +0100

    Run tomcat on an hadoop independent user

--- idm-webapp/src/main/java/idm/auth/ServerThread.java
+++ idm-webapp/src/main/java/idm/auth/ServerThread.java
@@ -1,12 +1,10 @@
 package idm.auth;
 
 
-import idiro.workflow.server.WorkflowPrefManager;
 import idm.useful.UserPrefManager;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
@@ -57,9 +55,6 @@ public class ServerThread{
 			run = true;
 			try {
 				list.add(this);
-				final String command = getBaseCommand(user, password, port) + " & echo $!";
-				logger.info("command to launch:\n"+command);
-				
 				Thread server = new Thread(){
 
 					@Override
@@ -80,6 +75,9 @@ public class ServerThread{
 					        Process pr = rt.exec("which java");
 							BufferedReader stdInput = new BufferedReader(
 									new InputStreamReader(pr.getInputStream()));
+					        
+					        final String command = getBaseCommand(user, password, port) + " & echo $!";
+							logger.info("command to launch:\n"+command);
 					        
 					        channel = session.openChannel("exec");
 					        String javahome = stdInput.readLine();
@@ -115,83 +113,40 @@ public class ServerThread{
 		}
 	}
 	
-	/** getBaseCommand
-	 * 
-	 * method to retrieve and generate the command line to be executed
-	 * 
-	 * @return String - command
-	 * @author Igor.Souza
-	 */
-	protected String getBaseCommand(String user,String password, int port){
+	private String getBaseCommand(String user, String password, int port){
+		String command = "";
+		try {
+			
+			File file = new File(UserPrefManager.getUserProperty("workflow_lib_path"));
 
-		String command = null;
+			// Reading directory contents
+			File[] files = file.listFiles();
 
-		File file = new File(UserPrefManager.getUserProperty("workflow_lib_path"));
+			StringBuffer path = new StringBuffer();
 
-		// Reading directory contents
-		File[] files = file.listFiles();
-
-		StringBuffer path = new StringBuffer();
-
-		for (int i = 0; i < files.length; i++) {
-			path.append(files[i] + ":");
+			for (int i = 0; i < files.length; i++) {
+				path.append(files[i] + ":");
+			}
+			String p = path.substring(0, path.length()-1);
+			
+			channel = session.openChannel("exec");
+			String c = "java -cp "+p+":"+ServerThread.class.getProtectionDomain().getCodeSource().getLocation().getPath().replace("idm/auth/ServerThread.class", "")+" idm.auth.BaseCommand "+port;
+			((ChannelExec)channel).setCommand(c);
+		    channel.connect();
+		    
+		    BufferedReader br = new BufferedReader(new InputStreamReader(channel.getInputStream()));
+		    String line;
+		    while ((line = br.readLine()) != null){
+		    	command += line;
+		    }
+	        channel.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		String p = path.substring(0, path.length()-1);
-		String packagePath = getPackageClasspath(WorkflowPrefManager.userPackageLibPath);
-		String classpath = " -classpath " + p + packagePath;
-
-		String codebase =  " -Djava.rmi.server.codebase="+getRMICodeBase();
-		String hostname = " -Djava.rmi.server.hostname="+getRMIHost();
-
-		command =  classpath + codebase + hostname + " idiro.workflow.server.connect.ServerMain "+port;
-
-		return command;
+	    return command;
 	}
+
 	
-	/** getRMICodeBase
-	 * 
-	 * method to retrieve the path of the main class
-	 * 
-	 * @return String
-	 * @author Igor.Souza
-	 */
-	protected String getRMICodeBase(){
-		String ans = null;
-		try {
-			InputStream is = getClass().getResourceAsStream( "/META-INF/application.properties" );
-			Properties prop = new Properties();
-			prop.load(is);
-
-			String inClasspath = UserPrefManager.getUserProperty("idiro_interface_path");
-
-			return "file:"+inClasspath;
-
-		}catch (Exception e) {
-			logger.warn("No RMI server codebase in application.properties, maybe set up in argument");
-		}
-		return System.getProperties().getProperty("java.rmi.server.codebase", ans);
-	}
-
-	/** getRMIHost
-	 * 
-	 * method to retrieve the server host name
-	 * 
-	 * @return String
-	 * @author Igor.Souza
-	 */
-	protected String getRMIHost(){
-		String ans = null;
-		try {
-			InputStream is = getClass().getResourceAsStream( "/META-INF/application.properties" );
-			Properties prop = new Properties();
-			prop.load(is);
-			ans = prop.getProperty("java.rmi.server.hostname");
-
-		}catch (Exception e) {
-			logger.warn("No RMI server codebase in application.properties, maybe set up in argument");
-		}
-		return ans == null ? "127.0.0.1" : ans;
-	}
 
 	/** kill
 	 * 
@@ -231,16 +186,5 @@ public class ServerThread{
 	 */
 	public final boolean isRun() {
 		return run;
-	}
-
-	private String getPackageClasspath(String path){
-		File f = new File(path);
-		String classPath = "";
-		if (f.exists()){
-			for (String file : f.list()){
-				classPath += ":"+path+"/"+file;
-			}
-		}
-		return classPath;
 	}
 }
