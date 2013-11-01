55ab5662f986f0888d4605b0322a3281b96d1c07
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2013-11-01 12:27:47 +0000

    Fix bug and add script for adding/removing packages.

---
+++ idm-webapp/src/main/resources/scripts/remove_package_system.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Script that deploy the war file into tomcat
+#Set up dynamic properties
+SCRIPT_LOCATION=${BASH_SOURCE[0]}
+SCRIPT_PATH="$(cd $(dirname "${SCRIPT_LOCATION}"); pwd -P)/$(basename "${SCRIPT_LOCATION}")"
+SCRIPT_PATH="${SCRIPT_PATH%/*}"
+
+PACKAGE_NAME=$1
+if [ -z $PACKAGE_NAME ]; then
+	echo "This script takes for argument the the package name to uninstall"
+	exit
+fi
+
+for i in ${SCRIPT_PATH}/../lib/*.jar ; do
+    CLASSPATH=$CLASSPATH:$i
+done
+
+MAIN_CLASS=idiro.workflow.utils.PackageManager 
+export SCRIPT_PATH PID
+
+JAVA_PATH=java
+$JAVA_PATH -server -classpath $CLASSPATH $MAIN_CLASS remove system $PACKAGE_NAME
