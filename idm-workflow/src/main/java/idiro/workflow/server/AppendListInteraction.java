76f34d49f7a46158acb4d9449ba362f988d6b2db
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2013-12-10 13:29:14 +0000

    Add functionalities to UserInteraction sub-classes.

--- idm-workflow/src/main/java/idiro/workflow/server/AppendListInteraction.java
+++ idm-workflow/src/main/java/idiro/workflow/server/AppendListInteraction.java
@@ -17,34 +17,68 @@ public class AppendListInteraction extends UserInteraction{
 
 	public AppendListInteraction(String name, String legend,
 			int column, int placeInColumn)
-			throws RemoteException {
+					throws RemoteException {
 		super(name, legend, DisplayType.appendList, column, placeInColumn);
 	}
-	
+
+	public void init() throws RemoteException{
+		Tree<String> list= null;
+		if(tree.isEmpty()){
+			list = tree.add("applist");
+			list.add("values");
+			list.add("output");
+		}
+	}
 
 	public List<String> getPossibleValues(){
 		return getPossibleValuesFromList();
 	}
 	
-	public List<String> getValues(){
+	public void setPossibleValues(List<String> values) throws RemoteException{
+		init();
+		Tree<String> vals = tree.getFirstChild("applist").getFirstChild("values");
+		vals.removeAllChildren();
+		Iterator<String> it = values.iterator();
+		while(it.hasNext()){
+			vals.add("value").add(it.next());
+		}
+	}
+	
+
+	public List<String> getValues() throws RemoteException{
+		init();
 		List<String> values = null;
-		if(display == DisplayType.appendList){
-			values = new LinkedList<String>();
-			List<Tree<String>> lRow = null;
-			Iterator<Tree<String>> rows = null;
-			try{
-				lRow = getTree()
-						.getFirstChild("applist").getFirstChild("output").getChildren("value");
-				rows = lRow.iterator();
-				while(rows.hasNext()){
-					values.add(rows.next().getFirstChild().getHead());
-				}
-			}catch(Exception e){
-				values = null;
-				logger.error("Tree structure incorrect");
+		values = new LinkedList<String>();
+		List<Tree<String>> lRow = null;
+		Iterator<Tree<String>> rows = null;
+		try{
+			lRow = getTree()
+					.getFirstChild("applist").getFirstChild("output").getChildren("value");
+			rows = lRow.iterator();
+			while(rows.hasNext()){
+				values.add(rows.next().getFirstChild().getHead());
 			}
+		}catch(Exception e){
+			values = null;
+			logger.error("Tree structure incorrect");
 		}
 		return values;
 	}
 	
+	public String setValues(List<String> values)  throws RemoteException{
+		String error = null;
+		init();
+		Tree<String> output =  tree.getFirstChild("applist").getFirstChild("output");
+		if(getPossibleValues().containsAll(values)){
+			output.removeAllChildren();
+			Iterator<String> it = values.iterator();
+			while(it.hasNext()){
+				output.add("value").add(it.next());
+			}
+		}else{
+			error = "Values "+values+" are not all available";
+		}
+		return error;
+	}
+
 }
