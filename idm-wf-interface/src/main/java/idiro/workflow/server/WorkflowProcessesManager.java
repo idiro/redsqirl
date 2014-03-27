bc970e30fc5b8dcc6dfb9aeb9455cd3146713093
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2014-03-27 13:36:31 +0000

    Correct small issue when killing idm-workflow process.

--- idm-wf-interface/src/main/java/idiro/workflow/server/WorkflowProcessesManager.java
+++ idm-wf-interface/src/main/java/idiro/workflow/server/WorkflowProcessesManager.java
@@ -9,26 +9,18 @@ public class WorkflowProcessesManager extends ProcessesManager {
 	/**
 	 * Constructor
 	 */
-	public WorkflowProcessesManager() {
 
-	}
 	/**
 	 * Get the latest instance of Process Manager for Workflow
 	 * @return {@link idiro.workflow.server.ProcessesManager} current instance for workflow
 	 * @throws IOException
 	 */
-	public ProcessesManager getInstance() throws IOException {
+	public WorkflowProcessesManager() throws IOException  {
 
 		fname = new String(WorkflowPrefManager.pathUserPref.get() + "/tmp/"
 				+ workflow_pid + "_processes.txt");
 		file = new File(fname);
-
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-		pid="";
 		loadPid();
-		return this;
 	}
 
 	
