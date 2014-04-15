705bcc6d01b36727fa841ea64626cb46c672497a
igor <igor.souza@idiro.com>
2014-04-15 14:29:45 +0100

    merge

--- idm-webapp/src/main/java/idm/ItemList.java
+++ idm-webapp/src/main/java/idm/ItemList.java
@@ -2,14 +2,9 @@ package idm;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
-import javax.faces.model.SelectItem;
 
 import org.richfaces.model.Ordering;
 
@@ -28,21 +23,16 @@ public class ItemList implements Serializable{
 	private String idSelected;
 	private String property;
 	private String value;
-	private List<SelectItem> objs = new LinkedList<SelectItem>();
-	private List<SelectItem> objsedit = new LinkedList<SelectItem>();
 	private String file = "N";
 	
-	/*
-	private Map<String, String> typeTableInteraction = new HashMap<String, String>();
-	private Map<String, String> nameValue = new HashMap<String, String>();
-	private Map<String, Ordering> sortingOrder = new HashMap<String, Ordering>();
-	private Map<String, Object> filterValue = new HashMap<String, Object>();
-	private Map<String, String> nameValueEdit = new HashMap<String, String>();
-	private Map<String, String> nameValueGrid = new HashMap<String, String>();
-	*/
-	
-	private Map<String, Boolean> nameIsConst = new HashMap<String, Boolean>();
-	private Map<String, Boolean> valueHasLineBreak = new HashMap<String, Boolean>();
+	private Map<String, String> typeTableInteraction = new LinkedHashMap<String, String>();
+	private Map<String, String> nameValue = new LinkedHashMap<String, String>();
+	private Map<String, Ordering> sortingOrder = new LinkedHashMap<String, Ordering>();
+	private Map<String, Object> filterValue = new LinkedHashMap<String, Object>();
+	private Map<String, String> nameValueEdit = new LinkedHashMap<String, String>();
+	private Map<String, Boolean> nameIsConst = new LinkedHashMap<String, Boolean>();
+	private Map<String, Boolean> valueHasLineBreak = new LinkedHashMap<String, Boolean>();
+	private Map<String, String> nameValueGrid = new LinkedHashMap<String, String>();
 	
 	/*
 	 *
@@ -63,27 +53,11 @@ public class ItemList implements Serializable{
 	}
 
 	public List<String> getKeyAsListNameValue(){
-		List<String> keys = new LinkedList<String>();
-		for(SelectItem ob :objs){
-			keys.add(ob.getLabel());
-		}
-		return keys;
-	}
-	
-	public List<SelectItem> getObjs(){
-		return this.objs;
-	}
-
-	public List<SelectItem> getObjsEdit(){
-		return this.objsedit;
+		return new ArrayList<String>(nameValue.keySet());
 	}
 	
 	public List<String> getKeyAsListNameValueEdit(){
-		List<String> keys = new LinkedList<String>();
-		for(SelectItem ob :objsedit){
-			keys.add(ob.getLabel());
-		}
-		return keys;
+		return new ArrayList<String>(nameValueEdit.keySet());
 	}
 	
 	public String getName() {
@@ -119,40 +93,35 @@ public class ItemList implements Serializable{
 	}
 
 	public Map<String, String> getNameValue() {
-		Map<String,String> nameValue = new LinkedHashMap<String,String>();
-		for(SelectItem ob : objs){
-			nameValue.put((String) ob.getValue() ,ob.getLabel());
-		}
 		return nameValue;
 	}
 
 	public void setNameValue(Map<String, String> nameValue) {
-		List<SelectItem> newNameValue = new LinkedList<SelectItem>();
-		Iterator<String> keySet = nameValue.keySet().iterator();
-		while(keySet.hasNext()){
-			String key = keySet.next();
-			newNameValue.add(new SelectItem(key,nameValue.get(key)));
-		}
-		this.objs = newNameValue;
+		this.nameValue = nameValue;
+	}
+	
+	public Map<String, Ordering> getSortingOrder() {
+		return sortingOrder;
+	}
+
+	public void setSortingOrder(Map<String, Ordering> sortingOrder) {
+		this.sortingOrder = sortingOrder;
+	}
+	
+	public Map<String, Object> getFilterValue() {
+		return filterValue;
+	}
+
+	public void setFilterValue(Map<String, Object> filterValue) {
+		this.filterValue = filterValue;
 	}
 
 	public Map<String, String> getNameValueEdit() {
-		Map<String,String> nameValue = new LinkedHashMap<String,String>();
-		for(SelectItem ob : objsedit){
-			nameValue.put((String) ob.getValue() ,ob.getLabel());
-		}
-		return nameValue;
+		return nameValueEdit;
 	}
-	
 
 	public void setNameValueEdit(Map<String, String> nameValueEdit) {
-		List<SelectItem> newNameValue = new LinkedList<SelectItem>();
-		Iterator<String> keySet = nameValueEdit.keySet().iterator();
-		while(keySet.hasNext()){
-			String key = keySet.next();
-			newNameValue.add(new SelectItem(key,nameValueEdit.get(key)));
-		}
-		this.objsedit = newNameValue;
+		this.nameValueEdit = nameValueEdit;
 	}
 	
 	public Map<String, Boolean> getNameIsConst() {
@@ -172,12 +141,13 @@ public class ItemList implements Serializable{
 	}
 
 	public Map<String, String> getTypeTableInteraction() {
-		return null;//typeTableInteraction;
+		return typeTableInteraction;
 	}
 
 	public void setTypeTableInteraction(Map<String, String> typeTableInteraction) {
-//		this.typeTableInteraction = typeTableInteraction;
+		this.typeTableInteraction = typeTableInteraction;
 	}
+	
 	public Map<String, Boolean> getValueHasLineBreak() {
 		return valueHasLineBreak;
 	}
@@ -186,6 +156,13 @@ public class ItemList implements Serializable{
 		this.valueHasLineBreak = valueHasLineBreak;
 	}
 
+	public Map<String, String> getNameValueGrid() {
+		return nameValueGrid;
+	}
+
+	public void setNameValueGrid(Map<String, String> nameValueGrid) {
+		this.nameValueGrid = nameValueGrid;
+	}
 
 	public String getProperty() {
 		return property;
