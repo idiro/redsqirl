b27fd17afa9dcf1073ceefb37e51e6867b8586c3
keith <keith.ellman@idiro.com>
2014-10-14 13:59:34 +0100

    page for uninstall SA

--- redsqirl-webapp/src/main/java/com/redsqirl/SubWorkflowManagerBean.java
+++ redsqirl-webapp/src/main/java/com/redsqirl/SubWorkflowManagerBean.java
@@ -2,8 +2,11 @@ package com.redsqirl;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
@@ -26,13 +29,17 @@ public class SubWorkflowManagerBean extends BaseBean implements Serializable{
 	
 	private String asSystem = "";
 	
+	//For selection uninstall
+	private List<SelectItem> uninstallUserSa = new ArrayList<SelectItem>(),	uninstallSysSa = new ArrayList<SelectItem>();
+	
+	//List of sub workflows
+	private List<String> userSA = new ArrayList<String>();
+	private List<String> systemSA = new ArrayList<String>();
+	
 	public void installCurrentSubWorkflow() throws RemoteException{
 		boolean ok = false;
 		
-		FacesContext context = FacesContext.getCurrentInstance();
-		UserInfoBean userInfoBean = (UserInfoBean) context.getApplication()
-				.evaluateExpressionGet(context, "#{userInfoBean}",
-						UserInfoBean.class);
+		
 		logger.info("subWorkflow name  "+name);
 		logger.info("subWorkflow name  "+actualName);
 		DataFlowInterface dfi = getworkFlowInterface();
@@ -41,7 +48,7 @@ public class SubWorkflowManagerBean extends BaseBean implements Serializable{
 		boolean system = asSystem.equals("System");
 		swa.setName("sa_"+name);
 		
-		String username = system ? null : userInfoBean.getUserName();
+		String username = system ? null : getUserInfoBean().getUserName();
 		
 		String error = saManager.install(username, swa, null);
 		
@@ -53,7 +60,10 @@ public class SubWorkflowManagerBean extends BaseBean implements Serializable{
 			request.setAttribute("msnError", "msnError");
 			logger.info(" "+error);
 		}else{
-//			MessageUseful.addInfoMessage("Install Success for "+swa.getName());
+			MessageUseful.addInfoMessage("Install Success for "+swa.getName());
+			HttpServletRequest request = (HttpServletRequest) FacesContext
+					.getCurrentInstance().getExternalContext().getRequest();
+			request.setAttribute("msnSuccess", "msnSuccess");
 			
 		}
 		ok = error ==null || error.isEmpty();
@@ -76,11 +86,8 @@ public class SubWorkflowManagerBean extends BaseBean implements Serializable{
 		boolean admin = false;
 		try{
 			logger.debug("is admin");
-			FacesContext context = FacesContext.getCurrentInstance();
-			UserInfoBean userInfoBean = (UserInfoBean) context.getApplication()
-					.evaluateExpressionGet(context, "#{userInfoBean}",
-							UserInfoBean.class);
-			String user = userInfoBean.getUserName();
+			
+			String user = getUserInfoBean().getUserName();
 			String[] admins = WorkflowPrefManager.getSysAdminUser();
 			if(admins != null){
 				for(String cur: admins){
@@ -116,6 +123,118 @@ public class SubWorkflowManagerBean extends BaseBean implements Serializable{
 	public void setAsSystem(String asSystem) {
 		this.asSystem = asSystem;
 	}
+	
+	public void refreshSubworkflowsAllList(){
+		refreshSubworkflowsSystemList();
+		refreshSubworkflowsUser();
+	}
+
+	
+	public void refreshSubworkflowsSystemList(){
+		systemSA = saManager.getAvailableSuperActions(null);
+		uninstallSysSa = new ArrayList<SelectItem>(systemSA.size());
+		setSystemSA(systemSA);
+	}
+	
+	public void refreshSubworkflowsUser(){
+		
+		userSA =  saManager.getAvailableSuperActions(getUserInfoBean().getUserName());
+		uninstallUserSa = new ArrayList<SelectItem>(userSA.size());
+		setUserSA(userSA);
+	}
+	
+	public void deleteSASystem(){
+		if(getAdmin()){
+			for(SelectItem saName : uninstallSysSa){
+				saManager.uninstall(null, saName.getLabel());
+			}
+		}
+	}
+	
+	public void deleteSaUser(){
+		String user = getUserInfoBean().getUserName();
+		for(SelectItem saName : uninstallUserSa){
+			saManager.uninstall(user, saName.getLabel());
+		}
+	}
+	
+	private UserInfoBean getUserInfoBean(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		UserInfoBean userInfoBean = (UserInfoBean) context.getApplication()
+				.evaluateExpressionGet(context, "#{userInfoBean}",
+						UserInfoBean.class);
+		
+		return userInfoBean;
+	}
+	
+
+
+	/**
+	 * @return the userSA
+	 */
+	public List<String> getUserSA() {
+		return userSA;
+	}
+
+
+	/**
+	 * @param userSA the userSA to set
+	 */
+	public void setUserSA(List<String> userSA) {
+		this.userSA = userSA;
+	}
+
+	
+
+	/**
+	 * @param systemSA the systemSA to set
+	 */
+	public void setSystemSA(List<String> systemSA) {
+		this.systemSA = systemSA;
+	}
+
+
+	/**
+	 * @return the systemSA
+	 */
+	public List<String> getSystemSA() {
+		return systemSA;
+	}
+
+
+	/**
+	 * @return the uninstallUserSa
+	 */
+	public List<SelectItem> getUninstallUserSa() {
+		return uninstallUserSa;
+	}
+
+
+	/**
+	 * @param uninstallUserSa the uninstallUserSa to set
+	 */
+	public void setUninstallUserSa(List<SelectItem> uninstallUserSa) {
+		this.uninstallUserSa = uninstallUserSa;
+	}
+
+
+	/**
+	 * @return the uninstallSysSa
+	 */
+	public List<SelectItem> getUninstallSysSa() {
+		return uninstallSysSa;
+	}
+
+
+	/**
+	 * @param uninstallSysSa the uninstallSysSa to set
+	 */
+	public void setUninstallSysSa(List<SelectItem> uninstallSysSa) {
+		this.uninstallSysSa = uninstallSysSa;
+	}
+
+	
+
 
 
 
