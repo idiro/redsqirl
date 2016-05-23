fb255e3ca2baeafcbba122f29c949c2cfeea3f7b
igor <igor.souza@idiro.com>
2016-05-23 14:28:27 +0100

    website

--- src/site/resources/javascript/analytics.js
+++ src/site/resources/javascript/analytics.js
@@ -688,3 +688,42 @@ function returnCorrectPath(path) {
 	return path;
 }
 
+
+function flyToElement(flyer, flyingTo) {
+	var $func = $(this);
+	var divider = 3;
+	var flyerClone = $(flyer).clone();
+	$(flyerClone).css({position: 'absolute', top: $(flyer).offset().top + "px", left: $(flyer).offset().left + "px", opacity: 1, 'z-index': 1000});
+	$('body').append($(flyerClone));
+	var gotoX = $(flyingTo).offset().left + ($(flyingTo).width() / 2) - ($(flyer).width()/divider)/2;
+	var gotoY = $(flyingTo).offset().top + ($(flyingTo).height() / 2) - ($(flyer).height()/divider)/2;
+	 
+	$(flyerClone).animate({
+		opacity: 0.4,
+		left: gotoX,
+		top: gotoY,
+		width: $(flyer).width()/divider,
+		height: $(flyer).height()/divider
+	}, 700,
+	function () {
+		$(flyingTo).fadeOut('fast', function () {
+			$(flyingTo).fadeIn('fast', function () {
+				$(flyerClone).fadeOut('fast', function () {
+					$(flyerClone).remove();
+				});
+			});
+		});
+	});
+}
+
+function addCart(obj){
+
+	//Scroll to top if cart icon is hidden on top
+	$('html, body').animate({
+		'scrollTop' : $(".cart_anchor").position().top
+	});
+	//Select item image and pass to the function
+	var itemImg = obj.parent().find('img').eq(0);
+	flyToElement($(itemImg), $('.cart_anchor'));
+}
+
