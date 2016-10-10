56f3c33545ae7ccd1fa1e58e9113944ab4e5ef49
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2016-10-10 17:42:27 +0100

    Add Copyright Header

--- redsqirl-workflow/src/main/java/com/redsqirl/workflow/server/connect/jdbc/HivePropertiesDetails.java
+++ redsqirl-workflow/src/main/java/com/redsqirl/workflow/server/connect/jdbc/HivePropertiesDetails.java
@@ -1,3 +1,22 @@
+/** 
+ *  Copyright © 2016 Red Sqirl, Ltd. All rights reserved.
+ *  Red Sqirl, Clarendon House, 34 Clarendon St., Dublin 2. Ireland
+ *
+ *  This file is part of Red Sqirl
+ *
+ *  User agrees that use of this software is governed by: 
+ *  (1) the applicable user limitations and specified terms and conditions of 
+ *      the license agreement which has been entered into with Red Sqirl; and 
+ *  (2) the proprietary and restricted rights notices included in this software.
+ *  
+ *  WARNING: THE PROPRIETARY INFORMATION OF Red Sqirl IS PROTECTED BY IRISH AND 
+ *  INTERNATIONAL LAW.  UNAUTHORISED REPRODUCTION, DISTRIBUTION OR ANY PORTION
+ *  OF IT, MAY RESULT IN CIVIL AND/OR CRIMINAL PENALTIES.
+ *  
+ *  If you have received this software in error please contact Red Sqirl at 
+ *  support@redsqirl.com
+ */
+
 package com.redsqirl.workflow.server.connect.jdbc;
 
 import org.apache.log4j.Logger;
